24c586e9a3315a5745ca378d67d3c92379232b2f
Erdi YILDIZ <erdiyildiz95@gmail.com>
2019-05-09 18:57:35 +0300

    test

--- demo/app.js
+++ demo/app.js
@@ -10,8 +10,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
 	e.preventDefault();
 	// Stash the event so it can be triggered later.
 	deferredPrompt = e;
-	// Update UI notify the user they can add to home screen
-	btnAdd.style.display = 'block';
   });
 
 // Progressive loading images
