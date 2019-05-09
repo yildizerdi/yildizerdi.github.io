3b9d8a548873c5989590eb82416a290b987e8a61
Erdi <erdiyildiz95@gmail.com>
2019-05-09 18:23:36 +0300

    Update sw.js

--- demo/sw.js
+++ demo/sw.js
@@ -7,13 +7,16 @@ self.addEventListener('install', function(e) {
   );
 });
 
-// Fetching content using Service Worker
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(r) {
       console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then(function(response) {
-        
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
       });
     })
   );
