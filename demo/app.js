eec4b7a59649bbc5bbbdf055e262674897e43d83
Erdi YILDIZ <erdiyildiz95@gmail.com>
2019-05-09 18:49:04 +0300

    test

--- demo/app.js
+++ demo/app.js
@@ -3,6 +3,17 @@ if('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('sw.js');
 };
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+	// Prevent Chrome 67 and earlier from automatically showing the prompt
+	e.preventDefault();
+	// Stash the event so it can be triggered later.
+	deferredPrompt = e;
+	// Update UI notify the user they can add to home screen
+	btnAdd.style.display = 'block';
+  });
+
 // Progressive loading images
 var imagesToLoad = document.querySelectorAll('img[data-src]');
 var loadImages = function(image) {
