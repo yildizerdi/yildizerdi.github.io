19d40ca8c864c7352367b9a66064340a4f22b838
Erdi YILDIZ <erdiyildiz95@gmail.com>
2019-05-10 12:47:16 +0300

    test

--- demo/sw.js
+++ demo/sw.js
@@ -16,17 +16,17 @@ self.addEventListener('install', function(e) {
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
-});
+// self.addEventListener('fetch', function(e) {
+//   e.respondWith(
+//     caches.match(e.request).then(function(r) {
+//       console.log('[Service Worker] Fetching resource: '+e.request.url);
+//       return r || fetch(e.request).then(function(response) {
+//         return caches.open(cacheName).then(function(cache) {
+//           console.log('[Service Worker] Caching new resource: '+e.request.url);
+//           cache.put(e.request, response.clone());
+//           return response;
+//         });
+//       });
+//     })
+//   );
+// });
